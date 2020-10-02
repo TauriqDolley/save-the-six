3647ec316b4240ef889149086b21a0d36a6180e1
unknown <tauriqdolley@gmail.com>
2020-10-02 17:15:59 +0200

    Replaced background-images with img in HTML. Fixed responsive design for medium sized screens.

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ function contentToHtml(text) {
 		.map((paragraph) => `<p>${paragraph}</p>`)
 		.join("");
 }
+
 informationBlock.innerHTML = contentToHtml(
 	"The <b>Save The Six</b> campaign is a collaborative network of individuals, companies and creators with a vested interest in the survival of the District Six Museum. We’re calling on local artists, creators and photographers to join us in our efforts to raise funds for the museum through the means of a digital auction hosted by <b>Between 10&5</b> with the support of <b>Orms</b> and <b>BASA</b>. \n\nFor more information and to donate an artwork for the auction please click the button below. All proceeds go directly to the operating cost of the museum."
 );
